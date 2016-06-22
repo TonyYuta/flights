768968019155c36a342bbdf9d789f97e0787c82e
Tony <TonyYuta@users.noreply.github.com>
2016-06-22 11:56:43 -0700

    Update HomePageTest.java

--- src/test/java/com/cheapair/sqa/tests/HomePageTest.java
+++ src/test/java/com/cheapair/sqa/tests/HomePageTest.java
@@ -16,10 +16,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import com.cheapair.sqa.data.ReadFileData;
-import com.cheapair.sqa.pages.HomePage;
-import com.cheapair.sqa.pages.HotelsPage;
-
 /**
  * HomePageTest //ADDD (description of class)
  * <p>
